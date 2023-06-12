dfb8b6ae991d2a10d93eb68eb2ec3ee525fbdf45
unknown <mironorring@hotmail.com>
2023-06-12 20:31:17 +0300

    panel rotation and new scene

--- Assets/Samples/XR Hands/1.1.0/HandVisualizer/Scripts/GridToPanel.cs
+++ Assets/Samples/XR Hands/1.1.0/HandVisualizer/Scripts/GridToPanel.cs
@@ -16,11 +16,20 @@ public class GridToPanel : MonoBehaviour
         panel = panelManager.GiveMeAPanel();
         if(panel != null)
             gameObject.transform.parent = panel.transform;
+
+        if (panel != null) //fix
+            transform.localScale = new Vector3(1f * (transform.localScale.x - panel.transform.localScale.x),
+                1f * (transform.localScale.y - panel.transform.localScale.y), 1f * (transform.localScale.z - panel.transform.localScale.z));
     }
 
     private void Update()
     {
         if (panel != null)
+        {
             transform.position = panel.transform.position + desiredDistance;
+            transform.localRotation = panel.transform.parent.localRotation;
+            
+           // panel.transform.localScale = transform.localScale;
+        }
     }
 }
