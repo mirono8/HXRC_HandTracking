17a7d25689f5468fa6898cb778d97850a8abf635
unknown <mironorring@hotmail.com>
2023-11-22 16:36:37 +0200

    adjust height slider

--- Assets/Samples/XR Hands/1.1.0/HandVisualizer/Scripts/AdjustHeight.cs
+++ Assets/Samples/XR Hands/1.1.0/HandVisualizer/Scripts/AdjustHeight.cs
@@ -2,17 +2,36 @@ using HandData;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 public class AdjustHeight : MonoBehaviour
 {
     bool colliding;
+
+    public Slider slider;
+
+    public float sliderValue;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Vector3 adjustedPosition;
+
+
+    private void Start()
+    {
+        //slider = GetComponent<Slider>();
+
+        slider.onValueChanged.AddListener(delegate { SetFloat(slider.value); });
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!colliding)
         {
             if (other.CompareTag("FingerCollider"))
             {
-
 
                 var finger = other.gameObject.GetComponentInParent<TrackColliders>();
 
@@ -22,6 +41,7 @@ public class AdjustHeight : MonoBehaviour
                 {
                     hand.leftHand.isGrabbing = true;
                     colliding = true;
+                    Debug.Log("collided with ghostwall");
 
                 }
                 else if ((int)hand.rightHand.myHandedness == (int)finger.GetMyHandedness(finger))
@@ -29,19 +49,28 @@ public class AdjustHeight : MonoBehaviour
                     hand.rightHand.isGrabbing = true;
                     colliding = true;
 
-
+                    Debug.Log("collided with ghostwall");
                 }
             }
         }
     }
+    private void Update()
+    {
+        SetFloat(slider.value);
+    }
 
-    private void OnTriggerExit(Collider other)
+    private void FixedUpdate()
+    {
+        target.transform.position = adjustedPosition;
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("FingerCollider"))
         {
-            var finger = other.gameObject.GetComponentInParent<TrackColliders>();
+            TrackColliders finger = other.gameObject.GetComponentInParent<TrackColliders>();
 
-            var hand = FindObjectOfType<HandDataOut>();
+            HandDataOut hand = FindObjectOfType<HandDataOut>();
 
             if ((int)hand.leftHand.myHandedness == (int)finger.GetMyHandedness(finger))
             {
@@ -54,8 +83,14 @@ public class AdjustHeight : MonoBehaviour
             {
                 hand.rightHand.isGrabbing = false;
                 colliding = false;
+                adjustedPosition = target.transform.position + new Vector3(target.transform.position.x, sliderValue, target.transform.position.z);
 
             }
         }
+    }
+
+    public void SetFloat(float value)
+    {
+        sliderValue = value;
     }
 }
